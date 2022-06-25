3dd9613c49533530292d7f1e20b00fc95fad3c1b
andtatur <anders.tatur@gmail.com>
2022-06-25 15:15:13 +0300

    Домашнее задание 5 и исправленное 4

--- Homeworks/04-JS/HOMEWORK/andtatur_dz/ifelse.js
+++ Homeworks/04-JS/HOMEWORK/andtatur_dz/ifelse.js
@@ -1,45 +1,51 @@
 // Перeделать данную конструкцию на swich/case решение
 
-if (darbaDiena === 'Monday') {
-    console.log('1');
-} else if (darbaDiena === 'Tuesday') {
-    console.log('2');
-} else if (darbaDiena === 'Wednestay') {
-    console.log('3');
-} else if (darbaDiena === 'Thursday') {
-    console.log('4');
-} else if (darbaDiena === 'Friday') {
-    console.log('5');
-} else if (darbaDiena === 'Saturday') {
-    console.log('6');
-} else if (darbaDiena === 'Sunday') {
-    console.log('7');
-} else {
-    console.log('Error');
-}
+
+
+// if (darbaDiena === 'Monday') {
+//     console.log('1');
+// } else if (darbaDiena === 'Tuesday') {
+//     console.log('2');
+// } else if (darbaDiena === 'Wednestay') {
+//     console.log('3');
+// } else if (darbaDiena === 'Thursday') {
+//     console.log('4');
+// } else if (darbaDiena === 'Friday') {
+//     console.log('5');
+// } else if (darbaDiena === 'Saturday') {
+//     console.log('6');
+// } else if (darbaDiena === 'Sunday') {
+//     console.log('7');
+// } else {
+//     console.log('Error');
+// }
 
 //переделываю
 
-switch (new Date().getDate()) {
-    case 1:
-        darbaDiena = "Monday";
+let darbaDiena = prompt("Kada darbadiena ir sodien?", "Monday")
+switch (darbaDiena) {
+    case "Monday":
+        console.log('1');
         break;
-    case 2:
-        darbaDiena = "Tuesday";
+    case "Tuesday":
+        console.log('2');
         break;
-    case 3:
-        darbaDiena = "Wednesday";
+    case "Wednesday":
+        console.log('3');
         break;
-    case 4:
-        darbaDiena = "Thursday";
+    case "Thursday":
+        console.log('4');
         break;
-    case 5:
-        darbaDiena = "Friday";
+    case "Friday":
+        console.log('5');
         break;
-    case 6:
-        darbaDiena = "Saturday";
+    case "Saturday":
+        console.log('6');
         break;
-    case 7:
-        darbaDiena = "Sunday";
+    case "Sunday":
+        console.log('7');
         break;
+    default:
+        console.log('Error');
+
 }
