068e86122be34d54cb379bab1ae8ba35d83849b9
Evgen <jevgeny.shkuratov@gmail.com>
2022-07-03 17:53:02 +0300

    homework 04

---
+++ Homeworks/04-JS/JevgenyShkuratov/cv.js
@@ -0,0 +1,40 @@
+// Создать объект с данными для страницы CV
+
+const pageData = {
+    firstName: "Jevgenijs",
+    secondName: "Shkuratovs",
+
+    workExperiece: {
+        place_001: "Operator - Production / manufacturing Oct 2018 – Currently working",
+        place_002: "Auto electrician, car dismantling - Service / Service industry Oct 2003 – Sep 2018 (almost 15 years)"
+    },
+
+    education: {
+        specialEducation: "Mācību centrs “RIMAN” Vocational 2009 – 2010",
+        tecknicalEducation: "Liepājas jūrniecības koledža Vocational, Technical engineering, Mechanics 1998 – 2003",
+        basic: "46 vidusskola Basic 1990 – 1998"
+    },
+
+    skills: [
+        "HTML",
+        "CSS3",
+        "JavaScript",
+        "SQL",
+        "PHP",
+        "Figma",
+        "Java"
+    ],
+
+    languages: [
+        "English",
+        "Latvian",
+        "Russian"
+    ],
+
+    contact: {
+        fullName: "Jevgenijs Shkuratovs",
+        tell: "+371 29924464",
+        email: "jevgeny.shkuratov@gmail.com",
+        linkedin: "https://www.linkedin.com/in/jevgeny-shkuratov/"
+    }
+}
