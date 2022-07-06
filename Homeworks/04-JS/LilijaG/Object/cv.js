1d7750ccd6765852a8fef8222be5cbaf68cb62ea
LilijaG <lgj@inbox.lv>
2022-07-06 03:17:22 +0300

    04-JS-Homeworks-LilijaG

---
+++ Homeworks/04-JS/LilijaG/Object/cv.js
@@ -0,0 +1,53 @@
+// Создать объект с данными для страницы CV
+
+const pageData = {
+    pageTitle: [
+    'DARBA PIEREDZE',
+    'IZGLĪTĪBA',
+    'PAPILDUS IZGLĪTĪBA, KURSI',
+    'CITAS PRASMES'
+    ]   
+}
+
+const person = {
+
+    firstName: 'Lilija', 
+    lastName: 'G.',
+    phone: '+371 29375097',
+    email: 'lgj@inbox.lv',
+    
+    periodFrom: [
+        '09.1996.',
+    ],
+    periodTo: [
+        'šodien',
+    ],    
+    office: [
+        'programmētāja',
+        'inženiere-programmētāja'
+    ] ,   
+    companies: [ 
+        'A/S “VENTBUNKERS”', 
+        'A/S “Baltijas Ekspresis”', 
+        'SIA “NAVISOFT”', 
+        'SIA “OSTAS CELTNIEKS”', 
+        'SIA “E-PASAULE”', 
+        'SIA “ŪDEKA”', 
+        'SIA “R.E.D. PRO”', 
+        'A/S “VENCEB”',
+        'SIA “VENTAMARINE”',
+        'A/S “HANSABANKA”',
+        'A/S “Ventspils Apvienotā Baltijas Banka”',
+        'SIA “VENOIL Ltd.”',
+        'Latvijas Banka “Vindavas” nodaļā',
+        'Ventspils Statistikas Pārvaldes skaitļošanas centrā',
+        'RPI Automatizētās Sistēmas vadības katedrā'
+    ],
+    workType: [
+        'amatu apvienošanas kārtībā',
+        'pastavīgā darbā'
+    ]
+    
+}
+
+
