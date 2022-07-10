4a92c503aafa7cc46309857a1c64a39d79d504bf
Evgen <jevgeny.shkuratov@gmail.com>
2022-07-10 20:57:12 +0300

    validation namber

--- Homeworks/05-JS/HOMEWORK/JevgenyShkuratov/script.js
+++ Homeworks/05-JS/HOMEWORK/JevgenyShkuratov/script.js
@@ -1,8 +1,14 @@
 let characters = ['', 'a', 'b', 'c', 'd', 'i', 'f', 'g'];
 let question_mumber = prompt('Enter numder from 1 to 7');
+
 if (promt = isNaN(question_mumber)) {
     alert('enter only number');
 }
+
+if (question_mumber > (characters.length - 1)) {
+    alert('the number must not be greater than 7');
+}
+
 let mainElement = document.getElementById('main');
 
 mainElement.innerText = characters[question_mumber];
