eddcb10b036c92e614321022f60fb7d7439de1d1
JevgenijVoronov <jevgenijsvoronovs@gmail.com>
2022-07-14 18:07:51 +0300

    adjsut repo

---
+++ Homeworks/07-Form-js/ API/dogApp/solution/app.js
@@ -0,0 +1,24 @@
+// const dogSelector   = document.getElementById('dogSelector');
+// const dogimg        = document.getElementById('dogImg');
+
+// function renderSelectOptions(data) {
+//     data.forEach(breed => {
+//         dogSelector.innerHTML += `<option value=${breed.value}>${breed.name}</option>`
+//     })
+// } 
+
+// function renderDogData(data) {
+//     dogImg.innerHTML = `<img src='${data.message}'>`
+// }
+
+// function getDogImg() {
+//     const url = `https://dog.ceo/api/breed/${dogSelector.value}/images/random`;
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(data => renderDogData(data))
+//         .catch(error => alert(error));
+// }
+
+// renderSelectOptions(data);
+// dogSelector.addEventListener('change', getDogImg);
---
+++ Lectures/08-JS-Forms/Class/js/Person.js
@@ -0,0 +1,79 @@
+class Car {
+    constructor() {};
+}
+
+class Car {
+    constructor(brand, model) {
+	this.brand = brand;
+	this.model = model;
+    };
+}
+
+let myCar = new Car("Volvo", "S60");
+
+console.log(myCar.model) // S60
+
+
+
+// class Person {
+//   constructor(name, age, email) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+//   }
+
+//   getInfo() {
+//     return `My name is ${this.name}, I'm ${this.age} old. You can contact me via ${this.email}`;
+//   }
+
+//   getEmail() {
+//     return this.email;
+//   }
+// }
+
+// class Male extends Person {
+//   constructor(name, age, email) {
+//     super(name, age, email);
+//     this.gender = "male";
+//   }
+
+//   getGender() {
+//     return this.gender;
+//   }
+// }
+
+// const male = new Male("Arturs", 23, "arturs@gmail.com");
+
+// class Female extends Person {
+//   constructor(name, age, email) {
+//     super(name, age, email);
+//     this.gender = "female";
+//   }
+
+//   getGender() {
+//     return this.gender;
+//   }
+// }
+
+// class Vehicle {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// class Model extends Vehicle {
+//   constructor(name, model) {
+//     super(name);
+//     this.model = model;
+//   }
+
+//   getInfo() {
+//     return `${this.name} is ${this.model}`;
+//   }
+// }
+
+// const bmw = new Model("bmw", "730d");
